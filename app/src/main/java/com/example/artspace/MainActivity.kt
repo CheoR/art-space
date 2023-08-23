2b12e804d59ef3e1e4ae77f031d4d90acb9d3d83
CheoR <cheo.roman+github@gmail.com>
2023-08-23 12:35:34 -0500

    style: button row spacing

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -52,8 +54,9 @@ class MainActivity : ComponentActivity() {
 fun ButtonRow(modifier: Modifier = Modifier) {
     Row(
         modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+//            .background(Color.Red)
+            .padding(horizontal = 24.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Button(
@@ -110,10 +113,12 @@ fun ImageRow(modifier: Modifier = Modifier) {
     var contentDescription = stringResource(R.string.image_description_1)
     Surface(
         modifier = Modifier
-            .background(color=Color.Transparent)
-            .fillMaxWidth()
-            .height(300.dp)
+            .background(color = Color.Transparent)
+            .padding(24.dp)
             .clip(RoundedCornerShape(8.dp)),
+//            .fillMaxWidth()
+//            .height(300.dp)
+//            .clip(RoundedCornerShape(8.dp)),
         color = Color.Transparent,
         shadowElevation = 4.dp,
         tonalElevation = 4.dp,
@@ -136,6 +141,7 @@ fun Screen(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .padding(8.dp),
         verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         ImageRow()
         InfoRow()
